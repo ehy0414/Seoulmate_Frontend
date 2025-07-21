01c096f27883f99c30743a3ab7aab4d3ac220a8e
ehy0414 <djagusdyd78@gmail.com>
2025-07-21 15:12:47 +0900

    design: 회원가입 디자인 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import SignUpPage from './pages/SignUpPage'
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SignUpPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/SignUpPage.tsx
@@ -0,0 +1,28 @@
+"use client";
+import * as React from "react";
+import { GoogleLoginButton } from "../components/signup/GoogleLoginButton";
+import { AppTitle } from "../components/signup/AppTitle";
+
+interface LoginScreenProps {
+  onGoogleLogin?: () => void;
+}
+
+export const SignUpPage: React.FC<LoginScreenProps> = ({ onGoogleLogin }) => {
+  const handleGoogleLogin = () => {
+    if (onGoogleLogin) {
+      onGoogleLogin();
+    } else {
+      // Default behavior - could integrate with actual Google OAuth
+      console.log("Google login clicked");
+    }
+  };
+
+  return (
+    <main className="box-border border border-gray-200 flex overflow-hidden flex-col items-center px-6 pt-6 pb-2 mx-auto w-full h-screen bg-white max-w-[480px]">
+      <AppTitle />
+      <GoogleLoginButton onClick={handleGoogleLogin} />
+    </main>
+  );
+};
+
+export default SignUpPage;
