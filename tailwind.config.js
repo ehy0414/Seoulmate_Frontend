7a03473e4400be375a99fdac298853dca065d986
dasosan <goodtony12@naver.com>
2025-07-28 17:44:05 +0900

    chore: clean up tailwind.config.js formatting and ensure consistent indentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html", 
+    "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      
+
       // 색상
       colors: {
         // 높은 숫자일수록 색이 진함
@@ -38,18 +38,18 @@ export default {
 
       // 폰트
       fontFamily: {
-      yangjin: ['yangjin'],
+        yangjin: ['yangjin'],
       },
 
       // 폰트 사이즈
       fontSize: {
-      'xs': ['0.75rem', { lineHeight: '1rem' }], // 0.75rem = 12px
-      'sm': ['0.875rem', { lineHeight: '1.25rem' }],// 0.875rem = 14px
-      'base': ['1rem', { lineHeight: '1.5rem' }], // 1rem = 16px
-      'lg': ['1.125rem', { lineHeight: '1.75rem' }], // 1.125rem = 18px
-      'xl': ['1.25rem', { lineHeight: '1.75rem' }], // 1.25rem = 20px
-      '2xl': ['1.5rem', { lineHeight: '2rem' }], // 1.5rem = 24px
-      '3xl': ['1.875rem', { lineHeight: '2.25rem' }] // 1.875rem = 30px
+        'xs': ['0.75rem', { lineHeight: '1rem' }], // 0.75rem = 12px
+        'sm': ['0.875rem', { lineHeight: '1.25rem' }],// 0.875rem = 14px
+        'base': ['1rem', { lineHeight: '1.5rem' }], // 1rem = 16px
+        'lg': ['1.125rem', { lineHeight: '1.75rem' }], // 1.125rem = 18px
+        'xl': ['1.25rem', { lineHeight: '1.75rem' }], // 1.25rem = 20px
+        '2xl': ['1.5rem', { lineHeight: '2rem' }], // 1.5rem = 24px
+        '3xl': ['1.875rem', { lineHeight: '2.25rem' }] // 1.875rem = 30px
       },
 
       // 글 간격
