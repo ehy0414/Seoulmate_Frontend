508486c3229b4336825cc6d1850a01f75be65df0
dasosan <goodtony12@naver.com>
2025-07-28 17:41:44 +0900

    feat: integrate vite-plugin-svgr for SVG handling and update TypeScript configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    svgr({
+      include: '**/*.svg?react', // ?react 쿼리 처리
+      svgrOptions: {
+        replaceAttrValues: {
+          '#AFA9A9': '{props.fill}',
+          '#F45F3A': '{props.fill}',
+        },
+      },
+    }),
+  ],
+});
