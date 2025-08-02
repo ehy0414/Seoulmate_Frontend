862362b9af59573a7bc9c50b957b7f97450c7b16
dasosan <goodtony12@naver.com>
2025-08-03 00:13:26 +0900

    feat: FloatingActionButton 클릭 핸들러를 onClick prop에서 내부 navigate로 변경

--- src/components/common/FloatingActionButton.tsx
+++ src/components/common/FloatingActionButton.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import PlusIcon from '../../assets/common/plus.svg';
 
 interface FloatingActionButtonProps {
-    onClick?: () => void;
     className?: string;
 }
 
-const FloatingActionButton: React.FC<FloatingActionButtonProps> = ({ onClick, className = '' }) => {
+const FloatingActionButton: React.FC<FloatingActionButtonProps> = ({ className = '' }) => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/create-meeting');
+    };
+
     return (
         <div className={`fixed bottom-[78px] right-[18px] ${className}`}>
             <button 
-                onClick={onClick}
+                onClick={handleClick}
                 className="w-[50px] h-[50px] bg-[#F45F3A] rounded-full flex items-center justify-center shadow-lg"
             >
                 <img src={PlusIcon} alt='플로팅 버튼' />
