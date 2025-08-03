e322109a1384aae3379e554ee56f2a7e44815620
김민지 <minzikx@sookmyung.ac.kr>
2025-08-03 15:47:18 +0900

    Feat: 일정이 없는 경우 예외처리 UI 추가

--- src/components/schedule/ScheduleList.tsx
+++ src/components/schedule/ScheduleList.tsx
@@ -1,4 +1,4 @@
-import type { Schedule }from "../../pages/schedule/Schedule";
+import type { Schedule } from "../../pages/schedule/Schedule";
 import ScheduleItem from "../../components/schedule/ScheduleItem";
 
 interface Props {
@@ -7,16 +7,27 @@ interface Props {
 }
 
 export default function ScheduleList({ date, schedules }: Props) {
+  const displayDate = new Date(date);
+  const month = displayDate.getMonth() + 1;
+  const day = displayDate.getDate();
+
   return (
     <div className="px-4 mt-6">
       <div className="text-base font-semibold mb-4">
-        {new Date(date).getMonth() + 1}월 {new Date(date).getDate()}일의 일정
+        {month}월 {day}일의 일정
       </div>
-      <div className="flex flex-col gap-3">
-        {schedules.map((schedule) => (
-          <ScheduleItem key={schedule.id} schedule={schedule} />
-        ))}
-      </div>
+
+      {schedules.length === 0 ? (
+        <div className="flex justify-center items-center h-[96px] p-2 bg-black-100 border border-black-400 rounded-md shadow-sm text-black-600 text-sm">
+          오늘은 일정이 없어요!
+        </div>
+      ) : (
+        <div className="flex flex-col gap-3">
+          {schedules.map((schedule) => (
+            <ScheduleItem key={schedule.id} schedule={schedule} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
