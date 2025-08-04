cd3e678337553dd04d63898cf2aabcb8c045a224
김민지 <minzikx@sookmyung.ac.kr>
2025-08-04 18:01:05 +0900

    Feat: TabMenu 컴포넌트 Schedule에 적용

--- src/pages/schedule/Schedule.tsx
+++ src/pages/schedule/Schedule.tsx
@@ -4,6 +4,7 @@ import CalendarGrid from '../../components/schedule/CalendarGrid';
 import ScheduleList from '../../components/schedule/ScheduleList';
 import { mockSchedules } from '../../mock/schedule/schedules';
 import BottomNavBar from '../../components/common/BottomNavBar';
+import TabMenu from '../../components/common/TabMenu';
 
 export interface Schedule {
   id: number;
@@ -16,18 +17,27 @@ export interface Schedule {
 
 export default function Schedule() {
   const todayDate = useMemo(() => new Date(), []);
+  const FIRST_TAB = '주최';
+  const SECOND_TAB = '참여';
 
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());
   const [schedules, setSchedules] = useState<Schedule[]>([]);
+  const [activeTab, setActiveTab] = useState<string>(FIRST_TAB);
 
   useEffect(() => {
     const selectedDateStr = selectedDate.toISOString().split('T')[0]; // "YYYY-MM-DD"
     setSchedules(mockSchedules.filter((s) => s.date === selectedDateStr));
   }, [selectedDate]);
-  
 
   return (
     <div className="relative min-h-screen bg-white pb-[100px]">
+      <TabMenu
+        firstTabText={FIRST_TAB}
+        secondTabText={SECOND_TAB}
+        activeTab={activeTab}
+        onFirstTabClick={() => setActiveTab(FIRST_TAB)}
+        onSecondTabClick={() => setActiveTab(SECOND_TAB)}
+      />
       <CalendarGrid
         schedules={mockSchedules}
         selectedDate={selectedDate}
