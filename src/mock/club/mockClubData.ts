f58da43120335121f70809926c2797662d3ea670
ehy0414 <djagusdyd78@gmail.com>
2025-08-08 14:45:25 +0900

    feat: mock데이터 추가 및 가져오기

---
+++ src/mock/club/mockClubData.ts
@@ -0,0 +1,50 @@
+// src/data/mockClubData.ts
+export const clubData = [
+  {
+    id: 1,
+    title: "페인팅 모임",
+    hobby: "예술",
+    location: "학식당 옆 잔디밭",
+    datetime: "7/8 12:00",
+    price: "무료",
+    description: "함께 모여 자유롭게 페인팅을 즐겨보는 시간!",
+    imageUrls: [
+      "https://api.builder.io/api/v1/image/assets/TEMP/8a697336a0668e6f2e3ffae0cb45c99d4b9d1681?width=320",
+      "https://api.builder.io/api/v1/image/assets/TEMP/69fcfc725ba28ecd840a947076aa7d0a62038622?width=786"
+    ],
+  },
+  {
+    id: 2,
+    title: "피아노 독학 모임",
+    hobby: "음악",
+    location: "정문 건너 자유레슨실",
+    datetime: "7/12 15:00",
+    price: "5,000원",
+    description: "함께 피아노를 배우고 연습해요!",
+    imageUrls: [
+      "https://api.builder.io/api/v1/image/assets/TEMP/8b57a445adaf2e0cb61b740abeae36eb3b3a9a54?width=320"
+    ],
+  },
+  {
+    id: 3,
+    title: "한국어 기초 클래스",
+    hobby: "언어",
+    location: "창신관 102호",
+    datetime: "7/29 12:00",
+    price: "20,000원",
+    description: "기초 한국어를 함께 배우는 시간입니다.",
+    imageUrls: [
+      "https://api.builder.io/api/v1/image/assets/TEMP/c470bd21cab6825806f1d59b81d84febb47dcfa2?width=320"
+    ],
+  },
+  {
+    id: 4,
+    title: "실생활 한국어 회화",
+    hobby: "언어",
+    location: "월당관 3502호",
+    datetime: "7/13 15:00",
+    price: "20,000원",
+    description: "실생활에서 쓰이는 한국어를 배워봐요!",
+    imageUrls: ["https://api.builder.io/api/v1/image/assets/TEMP/344c6a924c437317e8a762e0972dbf9e9d225456?width=320"],
+  },
+];
