28277e946f1da99682bf74fb699e302fedaa77cd
김민지 <minzikx@sookmyung.ac.kr>
2025-08-25 21:49:26 +0900

    Feat: date.ts 함수에서 날짜 로직 수정

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,1 +1,7 @@
-export const getDateString = (date: Date) => date.toISOString().split('T')[0];
+export const getDateString = (date: Date) => {
+    const y = date.getFullYear();
+    const m = String(date.getMonth() + 1).padStart(2, '0');
+    const d = String(date.getDate()).padStart(2, '0');
+    return `${y}-${m}-${d}`;
+  };
+  
